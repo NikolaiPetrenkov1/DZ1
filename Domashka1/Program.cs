e244086643582e746d760bb5e3b05222d53724c3
Петренков Николай <@corp.itstep>
2017-12-02 12:03:31 +0600

    rekt

---
+++ Domashka1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Practica N1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ima_i_fam;
+            Console.WriteLine("Введите ваши имя и фамилию:\n");
+            ima_i_fam = Console.ReadLine();
+            Console.WriteLine("Ваши имя и фамилия:");
+            Console.WriteLine(ima_i_fam);
+            Console.ReadKey(true);
+        }
+    }
+}
+
---
+++ program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа:");
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            int с;
+            с = a + b;
+            Console.WriteLine("Сумма чисел равна:");
+            Console.WriteLine(с);
+            Console.ReadLine();
+        }
+    }
+}
